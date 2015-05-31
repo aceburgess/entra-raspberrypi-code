5701a46b70c7145fcf691bbb180cecb1cbb81878
echenique11 <echenique11@gmail.com>
2015-05-30 21:24:16 -0400

    updating status (opened-closed) from pi

--- entra.rb
+++ entra.rb
@@ -1,29 +1,53 @@
 require 'net/http'
 require 'artoo'
+require 'json'
 
 class EntraApp
-  
-  def initialize(url,params = nil)
+
+  attr_accessor  :sleep_seconds
+  attr_reader    :open_seconds
+
+  def initialize(user_key,params = nil)
+    url = "http://10.0.2.185:3000/user_client_keys/#{user_key}"
     @uri = URI(url)
     @uri.query = URI.encode_www_form(params) if params
+    @sleep_seconds = 2
+    @open_seconds = 5
   end
 
   def get_url
     puts "[#{Time.now}] - get - #{@uri} " 
-    # req.basic_auth 'user', 'pass'
     res = Net::HTTP.get_response(@uri)
     if res.is_a?(Net::HTTPSuccess)
-      # res.body
-      puts "success..."
-      return true
+      @response = JSON.parse(res.body)
     else
-      puts "[#{Time.now}] - something went wrong "
-      puts "[#{Time.now}] - response for #{@uri} : "
-      puts "[#{Time.now}] - #{res.code}"
-      puts "[#{Time.now}] - #{res.message}"
-      puts "[#{Time.now}] - #{res.class.name}"
-      return false
+      log_error res,@uri
     end
+  end
+
+  def can_open?
+    @response["open"]
+  end
+
+  def update_status status
+    url = "http://10.0.2.185:3000/client_keys/#{@response['key']}/status/#{status}"
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+    if res.is_a?(Net::HTTPSuccess)
+      puts "[#{Time.now}] - Door #{status} successfully"
+    else
+      log_error(res,uri)
+    end
+  end
+
+  private 
+
+  def log_error res,uri
+    puts "[#{Time.now}] - something went wrong "
+    puts "[#{Time.now}] - response for #{uri} : "
+    puts "[#{Time.now}] - #{res.code}"
+    puts "[#{Time.now}] - #{res.message}"
+    puts "[#{Time.now}] - #{res.class.name}"
   end
 
 end
@@ -33,15 +57,15 @@ device :led, :driver => :led, :pin => 11
 
 work do
   entra = EntraApp.new(ARGV[0])
-  sleep_seconds = 2
   while(true) do
-    sleep sleep_seconds
-    if entra.get_url
-      sleep_seconds = 2
-      puts "opening..."
-      led.toggle
-    else
-      sleep_seconds = 10
+    sleep entra.sleep_seconds
+    entra.get_url
+    if entra.can_open?
+      led.on
+      entra.update_status "opened"
+      sleep entra.open_seconds
+      led.off
+      entra.update_status "closed"
     end
   end
 end
